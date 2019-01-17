57702744eb863cd0e69bf4c9ae16323202e07a0a
Bhavith <bhavithk@gmail.com>
2019-01-17 14:45:19 +0100

    removed some hardcoding

--- gce/main.tf
+++ gce/main.tf
@@ -9,7 +9,7 @@ resource "google_compute_instance" "vm_instance" {
    machine_type = "f1-micro"
    allow_stopping_for_update = true
    service_account {
-      email = "tf-local-executor@sandbox-devops-2018.iam.gserviceaccount.com"
+      email = "${google_service_account.terraform_local_executor.account_id}@sandbox-devops-2018.iam.gserviceaccount.com"
       scopes = [ "cloud-platform" ]
    }
    boot_disk {
@@ -35,6 +35,6 @@ resource "google_service_account_iam_binding" "admin-account-iam" {
    service_account_id = "projects/sandbox-devops-2018/serviceAccounts/${google_service_account.terraform_local_executor.account_id}@sandbox-devops-2018.iam.gserviceaccount.com"
    role        = "roles/editor"
    members = [
-    "serviceAccount:tf-local-executor@sandbox-devops-2018.iam.gserviceaccount.com"
+    "serviceAccount:${google_service_account.terraform_local_executor.account_id}@sandbox-devops-2018.iam.gserviceaccount.com"
    ]
 }
